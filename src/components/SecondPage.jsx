d3b08b49d92963d68dc2c8c812db261e238b79c8
yomzeew <yomzeew@gmail.com>
2024-10-11 02:58:48 +0100

    update

--- src/components/SecondPage.jsx
+++ src/components/SecondPage.jsx
@@ -142,7 +142,7 @@ const SecondPage = () => {
             />
            <NavigationBution 
            icondirection={<FontAwesomeIcon icon={faArrowCircleDown} size="xl" color={primarycolortwo} />}
-           pagename={'getappage'}
+           pagename={'getapppage'}
            setActive={(value)=>setActive(value)}
            />
           </div>
